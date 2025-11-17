28b7c506f30544cde62741c726fe3b4a75971c38
Ebee1205 <spiverain02@gmail.com>
2025-11-17 16:14:43 +0900

    feat: 필요 페이지 추가

---
+++ src/pages/Info/Archive.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script setup>
+// ----- 선언부 ----- //
+import { onMounted, onUnmounted, ref, watch } from "vue";
+import { useRouter, useRoute } from "vue-router";
+
+const emit = defineEmits(['']);
+
+
+// ----- 라이프 사이클 ----- //
+onMounted(() => {
+  emit('hide-side-appbar');
+});
+
+onUnmounted(() => {
+
+});
+
+// ----- 함수 정의 ----- //
+
+
+</script> 
+
+<style scoped>
+
+</style>
