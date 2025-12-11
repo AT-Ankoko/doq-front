29a696e9ae6d88c682afd90be4dfeb8f0678542f
Ebee1205 <spiverain02@gmail.com>
2025-12-12 00:12:15 +0900

    feat: 아카이브 페이지만 오픈

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,7 @@
         variant="text"
         append-icon="mdi-chevron-down"
         @click="clickNavBtn('/about')"
+        disabled
       >
         About
       </v-btn>
@@ -67,6 +68,7 @@
         variant="text"
         append-icon="mdi-chevron-down"
         @click="clickNavBtn('/team')"
+        disabled
       >
         Team
       </v-btn>
@@ -75,6 +77,7 @@
         variant="text"
         append-icon="mdi-chevron-down"
         @click="clickNavBtn('/howtouse')"
+        disabled
       >
         How to Use
       </v-btn>
